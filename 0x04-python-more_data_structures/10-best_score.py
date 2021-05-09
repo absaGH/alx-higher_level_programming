8ec2b3e76ba1a2df791c7c563f76370366dc9116
root <absa3852@gmail.com>
2021-05-09 05:59:17 +0000

    MoreDataStructure

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if my_dict and len(a_dictionary):
+    if a_dictionary and len(a_dictionary):
         max = list(a_dictionary.keys())[0]
         for key in a_dictionary:
             if a_dictionary[key] > a_dictionary[max]:
