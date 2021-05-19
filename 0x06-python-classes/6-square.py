47d5ef3c796b35956c0fbc14e803088147f18a17
root <absa3852@gmail.com>
2021-05-19 09:00:00 +0000

    pythonclass

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -75,9 +75,8 @@ class Square:
         Returns:
             None
         """
-        if type(value) is not tuple or len(value) != 2 or \
-           type(value[0]) is not int or value[0] < 0 or \
+        if type(value) is not tuple or len(value) != 2 or
+           type(value[0]) is not int or value[0] < 0 or
            type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        self.__position = value
