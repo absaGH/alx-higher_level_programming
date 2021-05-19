1e310e850c94f72b1aa898b9e3ae3f2e2809105e
root <absa3852@gmail.com>
2021-05-19 06:51:11 +0000

    pythonexception

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#afe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+        return False
+    else:
+        return True
