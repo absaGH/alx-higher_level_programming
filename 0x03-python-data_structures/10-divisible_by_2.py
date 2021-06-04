cb78e10efea170fd306641fb7b4350facf253d8a
root <absa3852@gmail.com>
2021-06-04 02:09:37 +0000

    data structure

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,4 +4,4 @@ def divisible_by_2(my_list=[]):
     if my_list:
         for val in my_list:
             result_list.append(False if val % 2 else True)
-    return resutl_list
+    return result_list
