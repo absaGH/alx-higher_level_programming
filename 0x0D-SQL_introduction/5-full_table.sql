c8fa9d753086c86e05abb94a26881c63ef1a3719
root <absa3852@gmail.com>
2021-07-27 09:01:46 +0000

    SQL_intro

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
--- prints out the full description of the table first_table from the database hbtn_0c_0
+-- prints out the full description of first_table from the database hbtn_0c_0
 -- the database name will be passed as an argument
 
 SHOW CREATE TABLE `first_table`;
