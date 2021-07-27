65c7e3246acad41d3b3c8f8a60c1fc7cf787d465
root <absa3852@gmail.com>
2021-07-27 09:48:12 +0000

    SQL_intro

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- removes all records with a score <= 5 in second_table(hbtn_0c_0)
+-- the database name will be passed as an argument of the mysql command
+
+DELETE FROM second_table WHERE score <= 5;
