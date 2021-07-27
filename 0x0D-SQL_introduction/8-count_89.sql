cd29ac8705667fd5e4224759097b351136fa074e
root <absa3852@gmail.com>
2021-07-27 09:15:14 +0000

    SQL_intro

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- displays the number of records with id = 89 in first_table(hbtn_0c_0)
+-- the database name will be passed as an argument of the mysql command
+
+SELECT COUNT(*) FROM first_table WHERE id=89;
