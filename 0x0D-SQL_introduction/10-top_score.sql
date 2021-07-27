596d79c0c05053c892265ded1700e78d4e751739
root <absa3852@gmail.com>
2021-07-27 09:27:45 +0000

    SQL_intro

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- lists all records of the table second_table of the database hbtn_0c_0
+-- display both the score and the name (in this order)
+-- display records ordered by score(top first)
+-- the database name will be passed as an argument of the mysql command
+
+SELECT score, name FROM second_table ORDER BY score DESC;
