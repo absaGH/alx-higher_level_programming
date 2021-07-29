32dea01b8be564222e8300e356e2f1fbdfb3c858
root <absa3852@gmail.com>
2021-07-29 12:50:33 +0000

    More queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,4 @@
 -- creates the MySQL server user user_0d_1
--- user_0d_1 should have all privileges MySQL server
--- The user_0d_1 password should be set to user_0d_1_pwd
--- If the user user_0d_1 already exists, the script will not fail
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
--- 0x0E-SQL_more_queries/README.md
+++ 0x0E-SQL_more_queries/README.md
@@ -1,2 +1,4 @@
+#SQL - More queries
+
 The tasks in this part are an extension of the previous SQL tasks. In this
 part DCL and complex queries are the main focus.
