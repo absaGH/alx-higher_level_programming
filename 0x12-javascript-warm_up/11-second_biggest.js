dd2d473a8ef7b26a02e64dab6edb64fb964af9ce
absaGH <absa3852@gmail.com>
2021-08-03 11:58:09 +0000

    JavaScript_intro

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,8 +3,7 @@ let secondMax = 0;
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const vals = process.argv.slice(2);
-  vals.sort();
+  const vals = process.argv.slice(2).sort((a, b) => a - b);
   secondMax = vals[vals.length - 2];
   console.log(secondMax);
 }
