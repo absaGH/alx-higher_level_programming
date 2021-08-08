aacde6c5e083f857b1c3bd4f33fc7ce2a939b46f
root <absa3852@gmail.com>
2021-08-08 14:03:35 +0000

    Python_ORM

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
                                                                     argv[2],
                                                                     argv[3]),
                                                          pool_pre_ping=True)
-    Base.metadata.create_all(eng)
+    
     Session = sessionmaker(bind=eng)
     session = Session()
     states = session.query(State)
