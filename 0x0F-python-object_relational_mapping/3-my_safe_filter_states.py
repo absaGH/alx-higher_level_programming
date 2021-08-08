e466522d360027574ce40e5403a9c531ee2cd333
root <absa3852@gmail.com>
2021-08-08 14:29:39 +0000

    Python_ORM

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,17 +4,11 @@ python script that displays all states from the database hbtn_0e_0_usa where
 name matches the argument and is safe from MySQL injections
 """
 
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    records = cursor.fetchall()
-    for record in records:
-      if record[1] == argv[4]:
-        print(record)
-    cursor.close()
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
