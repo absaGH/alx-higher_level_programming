ff5418a721fc1a4d48fa949abbfacc905ae6aeb6
root <absa3852@gmail.com>
2021-08-24 15:49:56 +0000

    pythonNetowrk1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,7 +4,6 @@ displays commits made by user from the most recent to oldest of the repository
 """
 import requests
 from sys import argv
-
 if __name__ == '__main__':
     rs = requests.get('https://api.github.com/repos/{}/{}/commits'
                      .format(argv[2], argv[1]))
