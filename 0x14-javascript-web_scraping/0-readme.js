d2c8febe29ccba02399efa4709a0379653cc301f
absaGH <absa3852@gmail.com>
2021-11-08 11:08:33 +0000

    print file content

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const flname = require('flname');
-flname.readFile(process.argv[2], 'utf8', function (error, content) {
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
   console.log(error || content);
 });
